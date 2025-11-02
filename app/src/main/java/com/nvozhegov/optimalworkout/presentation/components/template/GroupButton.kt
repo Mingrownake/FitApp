9306c63e72fc14bcba18b8737a303ceeaf8e7133
Mingrow <66433414+Mingrownake@users.noreply.github.com>
2025-11-02 21:02:33 +0300

    Внесены изменения в бд. Добавлены упражнения в группы

--- app/src/main/java/com/nvozhegov/optimalworkout/presentation/components/template/GroupButton.kt
+++ app/src/main/java/com/nvozhegov/optimalworkout/presentation/components/template/GroupButton.kt
@@ -1,12 +1,9 @@
 package com.nvozhegov.optimalworkout.presentation.components.template
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -16,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -61,43 +57,22 @@ fun GroupButton(
     }
 }
 
-private sealed class MuscleGroup(
-    val title: String
-) {
-    data object Neck: MuscleGroup("Neck")
-    data object Trapezius: MuscleGroup("Trapezius")
-    data object Broadest: MuscleGroup("Broadest")
-    data object Extensors: MuscleGroup("Extensors")
-    data object Shoulders: MuscleGroup("Shoulders")
-    data object Chest: MuscleGroup("Chest")
-    data object Biceps: MuscleGroup("Biceps")
-    data object Triceps: MuscleGroup("Triceps")
-    data object Forearms: MuscleGroup("Forearms")
-    data object Press: MuscleGroup("Press")
-    data object Quadriceps: MuscleGroup("Quadriceps")
-    data object Cheek: MuscleGroup("Cheek")
-    data object BackBiceps: MuscleGroup("Back Biceps")
-    data object Gastrocnemius: MuscleGroup("Gastrocnemius")
-}
-
 private fun getResourcesNameByTitle(
     title: String
 ): Int {
     return when (title) {
         "Neck" -> R.drawable.neck
-        "Trapezius" -> R.drawable.trapezius
-        "Broadest" -> R.drawable.broadest
-        "Extensors" -> R.drawable.extensors
+        "Back" -> R.drawable.back
         "Shoulders" -> R.drawable.shoulders
         "Chest" -> R.drawable.chest
         "Biceps" -> R.drawable.biceps
         "Triceps" -> R.drawable.triceps
         "Forearms" -> R.drawable.forearms
-        "Press" -> R.drawable.press
-        "Quadriceps" -> R.drawable.quadriceps
-        "Cheek" -> R.drawable.cheek
-        "Back Biceps" -> R.drawable.backbiceps
-        "Gastrocnemius" -> R.drawable.gastrocnemius
+        "Core" -> R.drawable.core
+        "Quads" -> R.drawable.quadriceps
+        "Glutes" -> R.drawable.glutes
+        "Hamstrings" -> R.drawable.hamstrings
+        "Calves" -> R.drawable.calves
         else -> throw IllegalArgumentException("Muscle group not found")
     }
 }
